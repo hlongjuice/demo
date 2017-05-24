783fb8bed62136faffce41367901606b23471a37
Hlong <isis.everything@gmail.com>
2017-05-24 20:14:34 +0700

    mac

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SQLite, SQLiteObject } from "@ionic-native/sqlite";
-import { Storage } from "@ionic/storage";
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
@@ -12,15 +11,37 @@ export class HomePage {
   public name: any;
   constructor(
     public navCtrl: NavController,
-    private storage: Storage
+    private storage: Storage,
+    private sqlite: SQLite,
+    private db:any
   ) {
-     window.openDatabase("my.db", "1.0", "Cordova Demo", 200000).transaction(
-       (tx)=>{
-          tx.executeSql(
-            "CREATE TABLE IF NOT EXISTS date(id INTEGER PRIMARY KEY AUTOINCREMENT,date DATE)"
-          );
-       }
-     )
+    /*WebSql for Chrome*/
+   /* window.openDatabase("my.db", "1.0", "Cordova Demo", 200000).transaction(
+      (tx) => {
+        tx.executeSql(
+          "CREATE TABLE IF NOT EXISTS production_date(id INTEGER PRIMARY KEY AUTOINCREMENT,date DATE)"
+        );
+        tx.executeSql('');
+      }
+    )*/
+   db= window.openDatabase("my.db", "1.0", "Cordova Demo", 200000);
+
+    /*SQLite For Mobile*/
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
+
+
+        db.executeSql('create table danceMoves(name VARCHAR(32))', {})
+          .then(() => console.log('Executed SQL'))
+          .catch(e => console.log(e));
+
+
+      })
+      .catch(e => console.log(e));
+
   }
 
 }
