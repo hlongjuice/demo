afeaa8cea2e6c800c78b5a639d6f7ed3595dd0e8
hlongjuice <isis.juicy@gmail.com>
2017-06-20 15:21:38 +0700

    pc

--- src/services/weburl.service.ts
+++ src/services/weburl.service.ts
@@ -6,7 +6,7 @@ export class WebUrlService{
     constructor(
         // private url:string
     ){
-        this.url='http://192.168.43.81/stseafood/public'; 
+        this.url='http://192.168.1.3/stseafood/public'; 
     }
     
     getUrl(){
