335fe459ed2001a86506792475da72f447cbf4b2
Hlong <isis.everything@gmail.com>
2017-08-07 10:12:52 +0700

    mac

--- src/pages/human-resource/car/car-request/add-request/add-request.ts
+++ src/pages/human-resource/car/car-request/add-request/add-request.ts
@@ -18,12 +18,14 @@ export class AddRequestPage {
   items: string[];
   allEmployees: any[];
   requestEmployees: any[];
+  requestEmployee: any;
+  requestEmployeeID: any;
   divisions: any[];
   selectedDivision: number;
-  selectedPassengerDivision: number[];
   selectedEmployee: number;
   selectedCarType: number;
   rank: string;
+  rankID: number;
   startDate: string;
   endDate: string;
   startTime: string;
@@ -31,7 +33,8 @@ export class AddRequestPage {
   hasPassenger: boolean;
   passengers: any[];
   passengerNumber: number;
-  passengerRow: number[];
+  passengerInputs: any[];
+  row: number;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -43,63 +46,93 @@ export class AddRequestPage {
   ngOnInit() {
     this.hasPassenger = false;
     this.passengerNumber = 0;
-    this.passengerRow = [];
     this.passengers = [];
-    this.selectedPassengerDivision = [];
     this.startDate = this.dateService.getDate();
     this.endDate = this.dateService.getDate();
     this.startTime = this.dateService.getTime().currentTime;
     this.endTime = this.dateService.getTime().currentTime;
     this.allEmployees = this.navParams.data.employees;
     this.divisions = this.navParams.data.divisions;
-    console.log(this.allEmployees);
+    this.passengerInputs = [{
+      'divisions': this.divisions,
+      'employee': ''
+    }];
   }
-
 
   /* Get Employee who can request */
   getRequestEmployees() {
-    console.log(this.selectedDivision);
     this.requestEmployees = this.allEmployees.filter(item => {
       return item.division_id == this.selectedDivision && item.salary_type_id == 1 //1 is monthly
     });
-
-    console.log(this.requestEmployees);
-
+    let modal = this.modalCtrl.create('RequestEmployeeListPage', {
+      'employees': this.requestEmployees
+    })
+    modal.present();
+    modal.onDidDismiss(result => {
+      if (result) {
+        this.requestEmployee = result.name + ' ' + result.lastname;//recieve employee from modal
+        this.requestEmployeeID = result.em_id;
+        this.rank = result.rank.name;
+        console.log(result);
+      }
+    })
   }
 
+  /***** Passenger ********/
   /* Set To Passenger Input */
-  setPassengerInput(event) {
+  setPassengerInput(event, index) {
+    console.log(event)
     let modal = this.modalCtrl.create('PassengerListPage', {
       'passengers': this.passengers
     })
     modal.present();
     modal.onDidDismiss(result => {
       if (result) {
-        console.log(result);
+        //  this.selectedPassengers[index]=result.name+' '+result.lastname;
       }
     })
-    console.log(event);
-
   }
-  /* Passenger */
+
   /* Get Division Employee */
-  getDivisionEmployees(index) {
-    this.passengers = this.allEmployees.filter(item => {
-      return item.division_id == this.selectedPassengerDivision[index];
+  getDivisionPassengers(index:number) {
+    console.log(index);
+    let divisionPassenger = new Promise((resolve, reject) => {
+      this.passengers = this.allEmployees.filter(item => {
+        return item.division_id == this.passengerInputs[index].division;
+      })
+      resolve(this.passengers);
+    });
+    divisionPassenger.then((result) => {
+      let modal = this.modalCtrl.create('PassengerListPage', { 'passengers': result })
+      modal.present();
+      modal.onDidDismiss(result => {
+        if (result) {
+          console.log(index)
+          this.passengerInputs[0].employee = result.name + ' ' + result.lastname;
+          console.log(this.passengerInputs[index])
+        }
+      })
     })
-    console.log(this.passengers);
+
+
   }
 
   /* Set Passengers */
   setPassengers() {
-    console.log('In Set passenger')
-    this.passengerRow.push(0);
+    let newInput = {
+      'divisions': this.divisions,
+      'employee': ''
+    }
+    this.passengerInputs.push(newInput);
     this.passengerNumber++;
+    console.log(this.passengerInputs)
   }
 
   /* deletePassenger */
   deletePassenger(index) {
-    this.passengerRow.splice(index, 1);
+    this.passengerInputs.splice(index, 1);
+    this.passengerInputs.splice(index, 1);
+    this.passengerNumber--;
   }
 
 }
--- src/pages/human-resource/car/car-request/car-request.ts
+++ src/pages/human-resource/car/car-request/car-request.ts
@@ -2,7 +2,7 @@ import { DivisionService } from './../../../../services/division.service';
 import { EmployeeService } from './../../../../services/employee.service';
 import { DateService } from './../../../../services/date.service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the CarRequestPage page.
@@ -18,43 +18,50 @@ import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angu
 export class CarRequestPage {
 
 
-  allEmployees:any;
-  divisions:any;
+  allEmployees: any;
+  divisions: any;
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public dateService:DateService,
-    public modalCtrl:ModalController,
-    public employeeService:EmployeeService,
-    public divisionService:DivisionService
+    public dateService: DateService,
+    public modalCtrl: ModalController,
+    public employeeService: EmployeeService,
+    public divisionService: DivisionService,
+    public loaderCtrl: LoadingController
   ) {
   }
 
-  ngOnInit(){
+  ngOnInit() {
+    let loader=this.loaderCtrl.create({
+      content:'กำลังโหลดข้อมูล...'
+    })
+    loader.present();
     /* Get Employee */
     this.employeeService.getAllEmployeeWithOutPage()
-    .then(result=>{
-      this.allEmployees=result;
-    }).catch(err=>console.log(err))
-    /* Get Division */
-    this.divisionService.getDivision()
-    .then(result=>{this.divisions=result})
-    .catch(err=>{console.log(err)})
+      .then(result => {
+        this.allEmployees = result;
+        /* Get Division */
+        this.divisionService.getDivision()
+          .then(result => { 
+            this.divisions = result 
+            loader.dismiss()
+          })
+          .catch(err => { console.log(err) })
+      }).catch(err => console.log(err))
   }
 
   /*Get Details*/
-  getDetails(){
+  getDetails() {
 
   }
 
   /*Add Request*/
-  addRequest(){
-    console.log('Yo!!');
-    let addModal=this.modalCtrl.create('AddRequestPage'
-    ,{
-      'employees':this.allEmployees,
-      'divisions':this.divisions
-    });
+  addRequest() {
+    let addModal = this.modalCtrl.create('AddRequestPage'
+      , {
+        'employees': this.allEmployees,
+        'divisions': this.divisions
+      });
     addModal.present();
   }
 
